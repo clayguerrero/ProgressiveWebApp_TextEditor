e7fd0169a926184739b588dbce6b51ed94285b5c
clayg <claytondguerrero@gmail.com>
2023-12-19 15:46:03 -0600

    added WebpackPwaManifest plugin to config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,33 +1,50 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Text Editor',
+        template: "./index.html",
+        title: "Text Editor",
+      }),
+      new WebpackPwaManifest({
+        short_name: "JATE",
+        name: "Text Editor",
+        desciption: "Text Editor",
+        backgroundColor: "#7eb4e2",
+        themeColor: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+      new InjectManifest({
+        
       })
     ],
 
     module: {
-      rules: [
-
-      ],
+      rules: [],
     },
   };
 };
